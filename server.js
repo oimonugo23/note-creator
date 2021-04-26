2ab3502da4e8a2d9798e620d10c6162d60274228
Olisaeloka Imonugo <ollimmo@gmail.com>
2021-04-25 21:00:32 -0400

    update numero uno

---
+++ server.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+const express = require("express");
+const htmlroutes = require("./routes/htmlroutes");
+const apiroutes = require("./routes/apiroutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", apiroutes)
+app.use("/", htmlroutes)
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
