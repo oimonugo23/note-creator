7656600aeae8474ded304f3e22db2927a7c5f52d
Olisaeloka Imonugo <ollimmo@gmail.com>
2021-04-25 21:22:40 -0400

    we made progress

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log("hello world");
 const express = require("express");
 const htmlroutes = require("./routes/htmlroutes");
 const apiroutes = require("./routes/apiroutes");
@@ -8,7 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api", apiroutes)
-app.use("/", htmlroutes)
+app.use(express.static("public"));
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
